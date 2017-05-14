cccacb7549bab33a27ecbc5a8682011dc9ed2a5f
ZHE WANG <tonyzhe@foxmail.com>
2017-05-13 22:24:05 -0400

    Update prof_pic.js

--- src/prof_pic.js
+++ src/prof_pic.js
@@ -12,7 +12,7 @@ var mysql= require('mysql');
 var connection = mysql.createConnection({
     host     : '127.0.0.1',
     user     : 'root',
-    password : 'wangzhe1998',
+    password : '',
     database : 'prof_profile'
 });
 

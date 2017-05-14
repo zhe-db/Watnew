19846bf9b0e7d8fe792d8724b33e230c6fd6b804
ZHE WANG <tonyzhe@foxmail.com>
2017-05-13 22:24:47 -0400

    Update prof_profile.js

--- src/prof_profile.js
+++ src/prof_profile.js
@@ -18,7 +18,7 @@ var mysql= require('mysql');
 var connection = mysql.createConnection({
     host     : '127.0.0.1',
     user     : 'root',
-    password : 'wangzhe1998',
+    password : '',
     database : 'prof_profile'
 });
 
